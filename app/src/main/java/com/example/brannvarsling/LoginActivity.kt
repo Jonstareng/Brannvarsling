d153268595fa75f1d8b46dac7cb443bdd0aedde7
Toblh <46895037+Toblh@users.noreply.github.com>
2021-02-17 15:19:13 +0100

    Registrering. Funker ikke foreløping.

--- app/src/main/java/com/example/brannvarsling/LoginActivity.kt
+++ app/src/main/java/com/example/brannvarsling/LoginActivity.kt
@@ -1,26 +1,24 @@
 package com.example.brannvarsling
 
-import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 
 class LogInActivity : AppCompatActivity() {
     private lateinit var auth: FirebaseAuth
-    private lateinit var registerButton: Button
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
-
-
-        registerButton.setOnClickListener{
-            startActivity(Intent(this, SignUpActivity::class.java))
-        }
+        auth = Firebase.auth
 
 
     }
+
+
 }
+
+
--- app/src/main/java/com/example/brannvarsling/SignUpActivity.kt
+++ app/src/main/java/com/example/brannvarsling/SignUpActivity.kt
@@ -1,19 +1,53 @@
 package com.example.brannvarsling
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.auth.FirebaseAuth
 
 
 class SignUpActivity : AppCompatActivity() {
 
-    lateinit var db: FirebaseFirestore
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
 
-    db = FirebaseFirestore.getInstance()
 
     }
 }
+
+    /*
+    // Funksjon som oppretter bruker hvis vellykket og lagres i Firebase
+    private fun registrerBruker() {
+        val email = Email.text.toString()
+        val password = Password.text.toString()
+
+        if (email.isEmpty() || password.isEmpty()) {
+            Toast.makeText(this, "Vennligst fyll inn email/passord", Toast.LENGTH_SHORT).show()
+            return
+        }
+
+        FirebaseAuth.getInstance().createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener {
+                if (!it.isSuccessful) return@addOnCompleteListener
+
+                // else if vellykket
+                startActivity(Intent(this, LogInActivity::class.java))
+                Log.d("Main", "Vellykket opprettelse med uid: ${it.result?.user?.uid}")
+
+            }
+            .addOnFailureListener {
+                Log.d("Main", "Opprettelse av bruker feilet: ${it.message}")
+                Toast.makeText(this, "Opprettelse av bruker feilet: ${it.message}", Toast.LENGTH_SHORT).show()
+            }
+    }
+
+}
+
+class User(val uid: String, val Email: String, val Passord: String) {
+    constructor() : this("", "", "")
+}
+     */
