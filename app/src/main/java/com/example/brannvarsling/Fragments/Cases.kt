c41cba738077b8f77b0035a8e9e7f2325960adac
Markus <Markus.bjorn@hotmail.com>
2021-02-18 14:32:55 +0100

    case

--- app/src/main/java/com/example/brannvarsling/Fragments/Cases.kt
+++ app/src/main/java/com/example/brannvarsling/Fragments/Cases.kt
@@ -1,21 +1,33 @@
 package com.example.brannvarsling.Fragments
 
+import android.R.layout.simple_spinner_item
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import android.widget.Button
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.example.brannvarsling.R
+import com.example.brannvarsling.databinding.FragmentCasesBinding
 
 class Cases: Fragment() {
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_cases, container, false)
+    private lateinit var binding: FragmentCasesBinding
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
     }
-    
+       /* val caseChoice = arrayOf("Brannvarsling","Nødlys")
+        val arrayAdapter = context?.let { ArrayAdapter(it, simple_spinner_item, caseChoice) }
+        spinner.adapter = arrayAdapter
+        spinner.onItemSelectedListener = object :
+            AdapterView.OnItemSelectedListener {
+        override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+            when(position){
+            }
+        }
+
+    }
+*/
 }
