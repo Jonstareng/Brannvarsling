de23eacd7db06fefaff27329deb6d197ffd536b9
Dashern <dashrrn@gmail.com>
2021-02-25 17:21:10 +0100

    fikset navigering og design + lagt til skjema fragment

--- app/src/main/java/com/example/brannvarsling/Fragments/Home.kt
+++ app/src/main/java/com/example/brannvarsling/Fragments/Home.kt
@@ -1,6 +1,7 @@
 package com.example.brannvarsling.Fragments
 
 import android.os.Bundle
+import android.text.TextUtils.replace
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -34,12 +35,23 @@ class Home : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        val fragmentCases = Cases()
+        val fragmentCalendar = Calendar()
+        val fragmentForm = Form()
 
         binding.buttonCases.setOnClickListener {
-            findNavController().navigate(R.id.action_home_to_cases)
+            setCurrentFragment(fragmentCases)
         }
         binding.buttonCalendar.setOnClickListener {
-            findNavController().navigate(R.id.action_home_to_calendar)
+            setCurrentFragment(fragmentCalendar)
+        }
+        binding.buttonNySkjema.setOnClickListener{
+            setCurrentFragment(fragmentForm)
         }
     }
+    private fun setCurrentFragment(fragment: Fragment)=
+            parentFragmentManager.beginTransaction().apply {
+                replace(R.id.my_nav_host_fragment,fragment)
+                commit()
+            }
 }
