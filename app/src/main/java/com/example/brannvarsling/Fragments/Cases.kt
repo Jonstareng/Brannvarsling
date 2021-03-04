abba2b2f1b86d98febdffb0ef5b95d1e34fca714
Toblh <46895037+Toblh@users.noreply.github.com>
2021-03-04 11:59:40 +0100

    .

--- app/src/main/java/com/example/brannvarsling/Fragments/Cases.kt
+++ app/src/main/java/com/example/brannvarsling/Fragments/Cases.kt
@@ -4,23 +4,21 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
 import androidx.databinding.DataBindingUtil
-import androidx.databinding.DataBindingUtil.setContentView
 import androidx.fragment.app.Fragment
-import androidx.navigation.fragment.findNavController
 import com.example.brannvarsling.R
 import com.example.brannvarsling.databinding.FragmentCasesBinding
 
 class Cases: Fragment() {
     private lateinit var binding: FragmentCasesBinding
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+
+
+
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_cases, container, false)
         return binding.root
-        // Inflate the layout for this fragment
+
     }
+
 }
