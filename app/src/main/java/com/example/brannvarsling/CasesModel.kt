51b4a27f23e2a2b52d5a64361c74ff867738edaa
Toblh <46895037+Toblh@users.noreply.github.com>
2021-03-04 16:28:48 +0100

    blablabla

---
+++ app/src/main/java/com/example/brannvarsling/CasesModel.kt
@@ -0,0 +1,5 @@
+package com.example.brannvarsling
+
+// Denne må utarbeides mer med hva som kommer til å inneholde saker
+
+class CasesModel(val Type: String = "")
--- app/src/main/java/com/example/brannvarsling/Fragments/Cases.kt
+++ app/src/main/java/com/example/brannvarsling/Fragments/Cases.kt
@@ -6,19 +6,19 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.brannvarsling.R
 import com.example.brannvarsling.databinding.FragmentCasesBinding
 
 class Cases: Fragment() {
     private lateinit var binding: FragmentCasesBinding
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-
-
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
 
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_cases, container, false)
         return binding.root
-
     }
+
+
 
 }
