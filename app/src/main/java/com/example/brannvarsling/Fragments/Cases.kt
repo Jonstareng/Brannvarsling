d4b2ca7697bd2fc21f6de950b8a4f2212053505a
Toblh <46895037+Toblh@users.noreply.github.com>
2021-03-06 14:16:12 +0100

    blablabla

--- app/src/main/java/com/example/brannvarsling/Fragments/Cases.kt
+++ app/src/main/java/com/example/brannvarsling/Fragments/Cases.kt
@@ -6,19 +6,47 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
-import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.brannvarsling.CasesModel
 import com.example.brannvarsling.R
 import com.example.brannvarsling.databinding.FragmentCasesBinding
+import com.firebase.ui.firestore.FirestoreRecyclerAdapter
+import com.firebase.ui.firestore.FirestoreRecyclerOptions
+
 
 class Cases: Fragment() {
     private lateinit var binding: FragmentCasesBinding
+    private lateinit var adapter: CaseAdapter
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
 
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_cases, container, false)
         return binding.root
+
+
     }
 
+
+    // Firestore recycleradapter
+    class CaseAdapter(options: FirestoreRecyclerOptions<CasesModel>) :
+            FirestoreRecyclerAdapter<CasesModel, CaseAdapter.CaseAdapterViewHolder>(options) {
+
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CaseAdapterViewHolder {
+            return CaseAdapterViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.recyclerview_item, parent, false))
+
+        }
+
+        override fun onBindViewHolder(holder: CaseAdapterViewHolder, position: Int, model: CasesModel) {
+
+        }
+
+        class CaseAdapterViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+            // val Type = itemView(R.id.recyclerview_item_tittel)
+
+        }
+
+    }
 
 
 }
