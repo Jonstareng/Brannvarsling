8e6eaed6f012c66cf7875d13ddffb0a9a44d88c5
Markus <Markus.bjorn@hotmail.com>
2021-03-25 16:59:36 +0100

    case

--- app/src/main/java/com/example/brannvarsling/Fragments/Cases.kt
+++ app/src/main/java/com/example/brannvarsling/Fragments/Cases.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.RecyclerView
@@ -23,7 +25,27 @@ class Cases: Fragment() {
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_cases, container, false)
         return binding.root
 
+    }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+    private fun spinnerMenu() {
+        val caseChoice = arrayOf("Brannvarsling", "Nødlys")
+        val arrayAdapter = context?.let { ArrayAdapter(it, simple_spinner_item, caseChoice) }
+        binding.spinner.adapter = arrayAdapter
+        binding.spinner.onItemSelectedListener = object :
+                AdapterView.OnItemSelectedListener {
+
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                val text: String = parent?.getItemAtPosition(position).toString()
+
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+
+            }
+        }
     }
 
 
