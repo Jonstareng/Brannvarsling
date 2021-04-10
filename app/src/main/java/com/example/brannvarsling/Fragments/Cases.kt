f77395a154b6c27df65b05a78a75825efae9309b
Jon <jonstareng@hotmail.com>
2021-04-10 12:48:41 +0200

    Cases funksjoner er i boks

--- app/src/main/java/com/example/brannvarsling/Fragments/Cases.kt
+++ app/src/main/java/com/example/brannvarsling/Fragments/Cases.kt
@@ -14,6 +14,7 @@ import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.databinding.DataBindingUtil.setContentView
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentTransaction
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.brannvarsling.CasesModel
@@ -25,45 +26,48 @@ import com.firebase.ui.firestore.FirestoreRecyclerOptions
 import com.google.firebase.firestore.FirebaseFirestore
 
 
-class Cases: Fragment() {
+class Cases: Fragment(), RecyclerviewAdapter.OnItemClickListner{
     private lateinit var binding: FragmentCasesBinding
     private var db = FirebaseFirestore.getInstance()
+    private lateinit var idUse: String
     var adapterR: RecyclerviewAdapter? = null
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
 
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        //binding2 = DialogWindowBinding.inflate(inflater, container, false)
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_cases, container, false)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
         createRecyclerView()
         //spinnerMenu()
-        binding.addCases.setOnClickListener{
-            writeToDb()
+        binding.addCases.setOnClickListener {
+            showAddDialog()
         }
 
-
     }
-/*
-            private fun spinnerMenu() {
-                val caseChoice = arrayOf("Brannvarsling", "Nødlys")
-                val arrayAdapter = context?.let { ArrayAdapter(it, simple_spinner_item, caseChoice) }
-                binding.spinner.adapter = arrayAdapter
-                binding.spinner.onItemSelectedListener = object :
-                        AdapterView.OnItemSelectedListener {
-                        override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
-                            val text: String = parent?.getItemAtPosition(position).toString()
-            }
-                    override fun onNothingSelected(parent: AdapterView<*>?) {
 
-                    }
+    /*
+                private fun spinnerMenu() {
+                    val caseChoice = arrayOf("Brannvarsling", "Nødlys")
+                    val arrayAdapter = context?.let { ArrayAdapter(it, simple_spinner_item, caseChoice) }
+                    binding.spinner.adapter = arrayAdapter
+                    binding.spinner.onItemSelectedListener = object :
+                            AdapterView.OnItemSelectedListener {
+                            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                                val text: String = parent?.getItemAtPosition(position).toString()
                 }
-    }
- */
+                        override fun onNothingSelected(parent: AdapterView<*>?) {
+
+                        }
+                    }
+        }
+     */
     // Firestore recycleradapter
-    private fun createRecyclerView(){
+    private fun createRecyclerView() {
         val query = db.collection("Test")
 
         //Toast.makeText(context, "$", Toast.LENGTH_LONG).show()
@@ -71,39 +75,46 @@ class Cases: Fragment() {
                 .setQuery(query, FirebaseCases::class.java).build()
 
 
-        adapterR = RecyclerviewAdapter(option)
+        adapterR = RecyclerviewAdapter(option, this)
 
         binding.recyclerviewCase.layoutManager = LinearLayoutManager(context)
         binding.recyclerviewCase.adapter = adapterR
 
-
     }
+
     override fun onStart() {
         super.onStart()
         adapterR?.startListening()
     }
 
-     override fun onStop() {
+    override fun onStop() {
         super.onStop()
         adapterR?.stopListening()
     }
 
-
-    private fun writeToDb(){
-        //val costumer = binding.Costumer.text.toString()
-        //val type = binding.Type.text.toString()
-
-        val user: MutableMap<String, Any> = HashMap()
-        user["Customer"] = "customer"
-        user["Type"] = "type"
+    private fun showAddDialog() {
+        val dialogFragment = AddDialogFragment()
+        val fragmentManager = activity?.supportFragmentManager
+        val transaction = fragmentManager?.beginTransaction()
 
 
-// Add a new document with a generated ID
-        db.collection("Test")
-                .add(user)
-                .addOnSuccessListener { documentReference -> Log.d(TAG, "DocumentSnapshot added with ID: " + documentReference.id) }
-                .addOnFailureListener { e -> Log.w(TAG, "Error adding document", e) }
+        transaction?.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
+        transaction?.add(android.R.id.content, dialogFragment)?.addToBackStack(null)?.commit()
+    }
+
+    override fun onItemClick(id: String) {
+        idUse = id
+        Toast.makeText(requireContext(), id, Toast.LENGTH_LONG).show()
+        val dialogFragment = RecyclerviewDialogFragment(idUse)
+        val fragmentManager = activity?.supportFragmentManager
+        val transaction = fragmentManager?.beginTransaction()
+
+        transaction?.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
+        transaction?.add(android.R.id.content, dialogFragment)?.addToBackStack(null)?.commit()
     }
 
 
 }
+
+
+
--- app/src/main/java/com/example/brannvarsling/RecyclerviewAdapter.kt
+++ app/src/main/java/com/example/brannvarsling/RecyclerviewAdapter.kt
@@ -4,14 +4,13 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
-import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import com.example.brannvarsling.dataClass.FirebaseCases
 import com.firebase.ui.firestore.FirestoreRecyclerAdapter
 import com.firebase.ui.firestore.FirestoreRecyclerOptions
 
 
-class RecyclerviewAdapter(options: FirestoreRecyclerOptions<FirebaseCases>) :
+class RecyclerviewAdapter(options: FirestoreRecyclerOptions<FirebaseCases>, private val listner: OnItemClickListner) :
         FirestoreRecyclerAdapter<FirebaseCases, RecyclerviewAdapter.ViewHolder>(options) {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
@@ -23,10 +22,27 @@ class RecyclerviewAdapter(options: FirestoreRecyclerOptions<FirebaseCases>) :
         holder.type.text = data.Type
 
     }
-     class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+     inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView), View.OnClickListener {
        var tittel: TextView = itemView.findViewById(R.id.recyclerview_item_title)
          var type: TextView = itemView.findViewById(R.id.recyclerview_item_type)
 
+         init {
+             itemView.setOnClickListener(this)
+         }
+
+         override fun onClick(v: View?) {
+             val position = adapterPosition
+             val pos = snapshots.getSnapshot(adapterPosition)
+             val positionId = pos.id
+             if (position != RecyclerView.NO_POSITION){
+                 listner.onItemClick(positionId)
+             }
+
+         }
+
+     }
+    interface OnItemClickListner{
+        fun onItemClick(id: String)
     }
 
 
