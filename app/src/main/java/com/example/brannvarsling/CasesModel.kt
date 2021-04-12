350768b49591464e124f4db30b564b943f25bc2d
Toblh <46895037+Toblh@users.noreply.github.com>
2021-04-12 16:21:29 +0200

    subknapper i floatingActionButton legger til views som den skal. Men disse legger seg ikke i riktig posisjon enda. Jobbes med!

--- app/src/main/java/com/example/brannvarsling/CasesModel.kt
+++ app/src/main/java/com/example/brannvarsling/CasesModel.kt
@@ -3,6 +3,12 @@ package com.example.brannvarsling
 // Denne må utarbeides mer med hva som kommer til å inneholde saker
 
 class CasesModel(
-        val Type: String = ""
-
+        var tittel: String = "",
+        var tiltakshaver: String = "",
+        var anleggssted: String = "",
+        var adresse: String = "",
+        var overforing: String = "",
+        var sporsmal: String = "",
+        var jaboks: Boolean = true,
+        var neiboks: Boolean= true
 )
